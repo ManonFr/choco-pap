f1e9628ce0054bdb48b4bb78060bbe52fabdd32e
ManonFr <manon.fromage@gmail.com>
2025-11-12 15:11:53 +0100

    Add responsive styles for mobile header and homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,21 +2,37 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import styles from "./Header.module.css";
 import { useCart } from "@/contexts/CartContext";
+import { useState } from "react";
+import styles from "./Header.module.css";
 
 export default function Header() {
   const pathname = usePathname();
   const { cart, openCart } = useCart();
   const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <Link href="/">Choco Pap</Link>
       </div>
-      <nav>
-        <ul className={styles.nav}>
+
+      <button
+        className={styles.burger}
+        onClick={toggleMenu}
+        aria-label="Ouvrir le menu mobile"
+      >
+        <i className="fa-solid fa-bars"></i>
+      </button>
+
+      <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ""}`}>
+        <ul>
           <li className={pathname === "/" ? styles.active : ""}>
             <Link href="/">Accueil</Link>
           </li>
